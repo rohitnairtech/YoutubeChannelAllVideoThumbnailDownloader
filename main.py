c6729249fad6d1cfcf44c236ca887371a884edf5
Rohit <puthenrohit@gmail.com>
2018-07-04 16:01:08 +0530

    Removed extra print statements and move all imports to the top

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-#Check if a local directory with the name 'thumbnails' exists else create one
 import os
+import urllib.request as urlReq
+import json
+
 if not os.path.exists('./thumbnails'):
     os.makedirs('./thumbnails')
 
-#Ask for the youtube channels ID
-
+#Ask for the youtube channels ID. Refer to this link: https://support.google.com/youtube/answer/3250431
 channel = input('Enter Channel ID: ')
-
-#Ask for the number of results that should be outputted if not answered then set default to 40
 
 fetch_limit = input('Max Results Per Page: (Set to 40 If Left Blank) ')
 DEFAULT_FETCH_LIMIT = 40
@@ -18,58 +17,25 @@ else :
     SET_FETCH_LIMIT = DEFAULT_FETCH_LIMIT
 
 #Get the API key, you can get it from here 'https://developers.google.com/youtube/v3/getting-started'
-
 API_KEY = input('Input your API-Key: ')
 
-#Build the GET request URL
-
 crafted_youtube_url = "https://www.googleapis.com/youtube/v3/search?key=" + API_KEY + "&channelId=" + channel + "&part=snippet,id&order=date&maxResults=" + str(SET_FETCH_LIMIT)
-
-#Build URL opener and add header
-
-import urllib.request as urlReq
 
 requester = urlReq.build_opener()
 requester.addheaders = [('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2311.135 Safari/537.36')]
 
-#Make the GET request
-
 response = requester.open(crafted_youtube_url)
-
-#Print the response header
-
-print (response.info())
-
-#Print response content (JSON)
 
 y_json = response.read()
 
-print(y_json)
-
-#Convert JSON to Pythonic Object
-
-import json
-
 py_object = json.loads(y_json)
 
-print(py_object)
-
-#Iterate through newly converted py_object
-
 for count, ob in enumerate(json_object['items']):
-	#Getting the item type. Only Proceeding if ItemType == video
 	itemType = ob['id']['kind']
 	if itemType == 'youtube#video':
-		print(ob)
-		print(count)
 		count += 1
-		#Print title of the video
 		print(ob['snippet']['title'])
-		#Print URL of the video
-		print(ob['snippet']['thumbnails']['high']['url'])
-		#Fetching the Image resource from the internet
 		resource = urlReq.urlopen(ob['snippet']['thumbnails']['high']['url'])
-		#Creating a new file (img) in binary write mode in the newly created folder
 		output = open("./thumbnails/thumbnail"+ str(count) +".jpg","wb")
-		#Write the image resource to this file
 		output.write(resource.read())
+		output.close()
