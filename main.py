436bc92e92517a7d1c50973b3c0cd772b3f988fb
Rohit <puthenrohit@gmail.com>
2018-07-04 11:39:37 +0530

    Ask for result fetch limit and set a default fetch

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 import os
 if not os.path.exists('./thumbnails'):
     os.makedirs('./thumbnails')
+
+#Ask for the youtube channels ID
+
+channel = input('Enter Channel ID: ')
+
+#Ask for the number of results that should be outputted if not answered then set default to 40
+
+fetch_limit = input('Max Results Per Page: (Set to 40 If Left Blank) ')
+DEFAULT_FETCH_LIMIT = 40
+
+if fetch_limit != "" :
+    SET_FETCH_LIMIT = fetch_limit
+else :
+    SET_FETCH_LIMIT = DEFAULT_FETCH_LIMIT
